baeef16e92edb13c39ecb5b614f8a345abeddedd
Md <shafikul.18288@gmail.com>
2023-06-28 08:51:47 +0600

    main js file some modifiation

--- js/main.js
+++ js/main.js
@@ -1,4 +1,28 @@
 
-const fast = 'fast content';
-const fastp = document.querySelector('p');
-fastp.innerHTML = 'fast check out this item'
+// const fast = 'fast content';
+// const fastp = document.querySelector('p');
+// fastp.innerHTML = 'fast check out this item';
+
+const es11checking = {
+    fast: 'shafikul',
+    second: 'runjila',
+    four: 'ami',
+    six: 'tumi',
+    dojona: {
+        runjila: 'kub  valo',
+        shafikul: 'akto o valo na',
+        differance: {
+            protom: 'mon ar mil',
+            detio: 'probar',
+            tetio: '......',
+            dojonarMil: {
+                valobasa: 'akto akto hoyto cilo',
+                vorosa: 'mon a hoy cilo na'
+            }
+        }
+    }
+}
+
+const collect = es11checking?.dojona?.differance?.dojonarMil?.valobasa;
+
+console.log(collect)
