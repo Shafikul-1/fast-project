96fc3d41b265d2635a473afa3b50e995e0ec0a55
Md <shafikul.18288@gmail.com>
2023-06-28 09:32:57 +0600

    simple edit somting file

---
+++ js/external.js
@@ -0,0 +1,4 @@
+// export const fastFN = 'This is  a demo content'
+//  export function add(){
+//     console.log(' Export the dynamic text')
+//  }
--- js/index.js
+++ js/index.js
@@ -1,1 +1,20 @@
-const fast = 'this is a demo contnet'
+// //nullash colosing oparator
+// // const nullclose = ''; 
+// const nullclose = 0;
+// const val = 'javaScript';
+
+// const result = nullclose ?? val;
+// const differance = nullclose || val;
+
+// console.log(result)
+// console.log(differance)
+
+
+//DYNAMIC IMPORT AND EXPORT
+
+// import ('./external.js')
+//     .then(({fastFN, add})=>{
+//        console.log(fastFN)
+//        add()
+//     })
+
